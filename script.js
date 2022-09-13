6db691867ce719a50003d939a5f992b6dd5ef0ac
Hamdan Salih <hamdansalihpc@gmail.com>
2022-09-13 22:38:36 +0530

    First structure

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
-console.log("happy bday");
+
 
 let myLibrary = [];
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click',addBookTOLibrary)
 
 function Book(title,author,pages,read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = read; // read is of boolean type
 }
+
+const harryPotter = new Book('Harry Potter', 'J.K.Rowling', 4100, true);
+myLibrary.push(harryPotter);
+const hobbit = new Book('Hobbit','J. R. R. Tolkien', 304, false);
+myLibrary.push(hobbit);
 
 function addBookTOLibrary(){
-    let title = prompt('title : ',);
-    let author = prompt('author : ',);
-    let pages = prompt('pages : ',);
-    let read = prompt('read : ',);
-    let book = new Book(title, author, pages, read)
+    const title = prompt('title : ',);
+    const author = prompt('author : ',);
+    const pages = prompt('pages : ',);
+    const read = prompt('read : ',);
+    
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+
+
+    for(item = 2; item < myLibrary.length; item++ ) {
+        let createDiv = document.createElement('div');
+        createDiv.classList.add('test');
+        createDiv.innerHTML = `<h2>${myLibrary[item].title}</h2>
+                            <span>${myLibrary[item].author}</span><br>
+                            <span>${myLibrary[item].pages}</span><br>
+                            <span>${myLibrary[item].read}</span>` ;
+        div_title.appendChild(createDiv)
+        console.log(item)
+    };
 }
 
+const div_title = document.getElementById('title');
+const div_author = document.getElementById('author');
+const div_pages = document.getElementById('pages');
+const div_read = document.getElementById('read');
 
 
-
+myLibrary.forEach(item => {
+    let createDiv = document.createElement('div');
+    createDiv.classList.add('test');
+    createDiv.innerHTML = `<h2>${item.title}</h2>
+                        <span>${item.author}</span><br>
+                        <span>${item.pages}</span><br>
+                        <span>${item.read}</span>` ;
+    div_title.appendChild(createDiv)
+    console.log(item)
+});
