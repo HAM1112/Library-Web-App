30bf36eaa66c81e6f1581ddb11a61412cd03448a
Hamdan Salih <hamdansalihpc@gmail.com>
2022-09-21 20:16:31 +0530

    added remove button for each cards

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
 
 
 let myLibrary = [];
-let main = document.getElementById('main');
-
-
-
-
 
 function Book(title,author,pages,read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    if(read == true){
-      this.read = "Read"  
-    }
-    else{
-        this.read = "Not read"
-    } // read is of boolean type
+    this.read = read  // read is of boolean type
 }
 
-const harryPotter = new Book('Harry Potter', 'J.K.Rowling', 4100, true);
+//const harryPotter = new Book('Harry Potter', 'J.K.Rowling', 4100, true);
 //myLibrary.push(harryPotter);
-const hobbit = new Book('Hobbit','J.R.R. Tolkien', 304, false);
+//const hobbit = new Book('Hobbit','J.R.R. Tolkien', 304, false);
 //myLibrary.push(hobbit);
-
-
 
 let SubmitBtn = document.getElementById('submitbtn');
 SubmitBtn.addEventListener('click',addBookTOLibrary)
@@ -39,75 +27,87 @@ function addBookTOLibrary(){
     if(title === "" || author == "" || pages ==""){
         return console.log("error")
     }
-    else{
-        
-        let book = new Book(title, author, pages, read);
-        myLibrary.push(book);
-        
-    }
-    createbook(title,author,pages,read)
+    
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    
+    render()
+    //createbook()
+    document.getElementById('book-form').reset()
     
 }
 //displayBooks()
+//render()
 
-function createbook(title,author,pages,read){
 
-    console.log("inside createBokks function");
-    let card = document.createElement('div');
-    card.classList.add('card')
 
+function createbook(item){
     
+    let main = document.getElementById('main');
+    let card = document.createElement('div');
     let card_title = document.createElement('h2')
-    card_title.innerHTML = `${title}`
+    let card_author = document.createElement('p')
+    let card_pages = document.createElement('p')
+    let card_read = document.createElement('button');
+    let card_remove = document.createElement('button');
+    
+    card.setAttribute('id', myLibrary.indexOf(item))
+    card.classList.add('card')
+    
+    card_title.innerHTML = `${item.title}`
     card.appendChild(card_title);
     
-    let card_author = document.createElement('p')
-    card_author.innerHTML = `Author : ${author}`
+    card_author.innerHTML = `Author : ${item.title}`
     card.appendChild(card_author)
     
-    let card_pages = document.createElement('p')
-    card_pages.innerHTML = `No.Pages : ${pages}`
+    card_pages.innerHTML = `No.Pages : ${item.pages}`
     card.appendChild(card_pages)
     
-    let card_read = document.createElement('button');
     card_read.setAttribute('id','ReadBtn');
-    if(read == false){
+    if(item.read == false){
         card_read.innerHTML = `Not Read`
         card_read.classList.add('readoff')
     }
     else{
         card_read.innerHTML = `Read`
         card_read.classList.add('readon')
+        //console.log(item.read);
     }
     card.appendChild(card_read);
     
     
     
-    let card_remove = document.createElement('button');
     card_remove.innerHTML = `Remove`;
     card_remove.setAttribute('id','removeBtn');
     card.appendChild(card_remove);
     
     // button event listener
     
-    let ReadBtn = document.getElementById('ReadBtn');
-    if(ReadBtn){
-        ReadBtn.addEventListener('click',()=>{
-            console.log("hi");
-        })
-    }
+    card_remove.addEventListener('click',()=>{
+        console.log("rm btn is working" + item);
+        myLibrary.splice(myLibrary.indexOf(item),1);
+        render()
+    })
+
+    // card_read.addEventListener('click',()=>{
+    //     item.read = !item.read;
+    //     render()
+    // })
     
-    
+    //render();
     main.appendChild(card);
+
 }
 
-
-function changeRead(){
-    console.log(read.checked)
+function render() {
+    const library = document.getElementById('main');
+    const card = document.querySelectorAll('.card');
+    card.forEach( card => library.removeChild(card));
+   
+    for (let i=0; i<myLibrary.length; i++){
+        createbook(myLibrary[i]);
+    }
 }
-
-
-
 
 
 
